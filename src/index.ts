85c57ed60d72206106bd838a331bce4cbc2afcd3
frank.yi <frank.yi@vretta.com>
2023-08-04 01:47:59 -0400

    added algorithm and thoughts

--- src/index.ts
+++ src/index.ts
@@ -27,10 +27,48 @@ interface Frame {
   y3: number;
 }
 
+// define a helper function dist(x1, y1, x2, y2) that gets the distance between two points
+// this will be helpful for finding the angle of the scissors
+function dist(x1: number, y1: number, x2: number, y2: number): number {
+  return Math.sqrt((x1 - x2)**2 + (y1 - y2)**2);
+}
+
 const calculateOpenings = (frames: Frame[]): number => {
-  const openings = 0;
+  var openings = 0;
 
   // Write your algorithm
+
+  // we define a boolean variable 'isOpen' to keep track of whether the scissors are currently open or not
+  // we can use this variable to see when the scissors go from open to close, and thus increment 1
+  var isOpen: boolean = false;
+
+  // process each frame individually
+  for (let i = 0; i < frames.length; i++) {
+    // First, find the dimensions of the triangle (we assume the two blades are equal)
+    const currentFrame = frames[i];
+    const legLength = dist(currentFrame.x1, currentFrame.y1, currentFrame.x2, currentFrame.y2);
+    const baseLength = dist(currentFrame.x2, currentFrame.y2, currentFrame.x3, currentFrame.y3);
+    // Using the leg length and base length, we can find the angle of the center of the scissor
+    const cosLaw = 1 - (baseLength**2)/(2 * legLength**2)
+    const angle = Math.acos(cosLaw);
+
+    // Let's define the closing threshold as 0 (completely closed), and the opening threshold
+    // as 30 degrees (or PI/6 rad)
+    // So to open a closed scissor, the scissor must go beyond 45 degrees, and to
+    // close a opened scissor, the scissor must completely close at 0 degrees
+    if (isOpen) {
+      // close if it has angle = 0
+      if (angle == 0) {
+        isOpen = false;
+        openings += 1;
+      }
+    } else if (!isOpen) {
+      // open if it has angle >= PI/2
+      if (angle >= Math.PI/4) {
+        isOpen = true;
+      }
+    }
+  }
 
   console.log(`**** Openings: ${openings} ****`);
   return openings;
